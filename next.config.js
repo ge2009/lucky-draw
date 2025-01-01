9fda8335bd31ff321e4ad0edd6b4c6ba43da305e
ge2009 <ge2009@gmail.com>
2025-01-01 17:00:39 +0800

    重新更新打包

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
+const TerserPlugin = require('terser-webpack-plugin');
+
 const nextConfig = {
   compiler: {
-    styledComponents: true, // 启用 styled-components
+    styledComponents: true,
   },
-  experimental: {
-    serverActions: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
   },
   typescript: {
     ignoreBuildErrors: true,
@@ -12,7 +15,54 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  webpack: (config) => {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.optimization = {
+        ...config.optimization,
+        minimize: true,
+        moduleIds: 'deterministic',
+        splitChunks: {
+          chunks: 'all',
+          minSize: 10000,
+          maxSize: 20000000,
+          cacheGroups: {
+            default: false,
+            vendors: false,
+            framework: {
+              name: 'framework',
+              test: /[\\/]node_modules[\\/](react|react-dom|scheduler|prop-types)[\\/]/,
+              priority: 40,
+              enforce: true,
+            },
+            lib: {
+              test: /[\\/]node_modules[\\/]/,
+              name: 'lib',
+              priority: 30,
+              minChunks: 1,
+              reuseExistingChunk: true,
+            },
+            commons: {
+              name: 'commons',
+              minChunks: 2,
+              priority: 20,
+            }
+          },
+        },
+      };
+
+      config.optimization.minimizer = [
+        ...config.optimization.minimizer,
+        new TerserPlugin({
+          terserOptions: {
+            compress: {
+              drop_console: true,
+            },
+            mangle: true,
+          },
+        }),
+      ];
+    }
+
     config.module.rules.push({
       test: /\.(mp3)$/,
       type: 'asset/resource',
@@ -20,8 +70,9 @@ const nextConfig = {
         filename: 'static/media/[name][ext]'
       }
     });
+
     return config;
-  }
+  },
 }
 
 module.exports = nextConfig 
